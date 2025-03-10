6f0b9b1536779ff3df8e59e9cb098120d09472af
rajpratap29 <therajpratapofficial@gmail.com>
2025-03-10 22:35:44 +0530

    Update: Learned about db schema, routes and controllers in backend

--- fullstack/index.js
+++ fullstack/index.js
@@ -3,6 +3,10 @@ import dotenv from "dotenv";
 import cors from "cors";
 import db from "./utils/db.js";
 
+// import all routes
+
+import userRoutes from "./routes/user.routes.js";
+
 dotenv.config();
 
 const app = express();
@@ -29,6 +33,9 @@ app.get("/raj", (req, res) => {
 
 // connect to db
 db();
+
+// user routes
+app.use("/api/v1/users", userRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
