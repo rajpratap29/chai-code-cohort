297fb875bb4a6c500984e948b2ab3c26c693798d
rajpratap29 <therajpratapofficial@gmail.com>
2025-03-17 23:19:36 +0530

    Update: Learning Authentication in SQL db

---
+++ fullstacksql/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const registerUser = async (req, res) => {
+    console.log("User registered");
+    await prisma.user.create({
+        
+    })
+};
