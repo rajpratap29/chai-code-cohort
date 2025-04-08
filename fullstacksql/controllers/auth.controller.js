b0990a6abe07d2b11d50d4c8db67a28f5207b059
rajpratap29 <therajpratapofficial@gmail.com>
2025-04-08 20:26:30 +0530

    Update: Learned authentication with prisma and postgressql

--- fullstacksql/controllers/auth.controller.js
+++ fullstacksql/controllers/auth.controller.js
@@ -1,10 +1,117 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
 export const registerUser = async (req, res) => {
-    console.log("User registered");
-    await prisma.user.create({
-        
-    })
+  const { name, email, password, phone } = req.body;
+
+  if (!name || !email || !password || !phone) {
+    console.log("Data is missing");
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
+
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exist",
+      });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const verificationToken = crypto.randomBytes(32).toString("hex");
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        hashedPassword,
+        verificationToken,
+      },
+    });
+
+    // res.status(200).json({
+    //   success: true,
+    //   message: "User registered successfully",
+    // });
+
+    // send mail - TODO
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "Registration failed",
+      error,
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const isMatch = bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    const cookieOptions = {
+      httpOnly: true,
+    };
+    res.cookie("token", token, cookieOptions);
+
+    return res.status(201).json({
+      success: true,
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      message: "Invalid email or password",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "Login failed",
+    });
+  }
 };
--- fullstacksql/routes/auth.route.js
+++ fullstacksql/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.controller";
+import { registerUser } from "../controllers/auth.controller.js";
 
 const userRouter = express.Router();
 
