ab2473dde6c8a57342f49953f29243ee257ddfab
rajpratap29 <therajpratapofficial@gmail.com>
2025-04-15 20:12:00 +0530

    Update: Learned about creating models in mega project

---
+++ megeproject/src/models/user.models.js
@@ -0,0 +1,60 @@
+import mongoose, { Schema } from "mongoose";
+
+const { userSchema } = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localpath: String,
+      },
+      default: {
+        url: `https://placehold.co/600x400`,
+        localpath: "",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    refreshToken: {
+      type: String,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
